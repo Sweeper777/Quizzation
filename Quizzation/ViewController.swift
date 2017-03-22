e1852b246832a29fc2436bc6bc1b680ba8dd996f
Sweeper777 <sumulang@gmail.com>
2017-03-22 06:51:48 +0000

    created a simple question

--- Quizzation/ViewController.swift
+++ Quizzation/ViewController.swift
@@ -6,6 +6,12 @@ class ViewController: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func test1() {
+        let testQuestion = Question(questionText: "Hello", possibleAnswers: ["a", "b"])
+        print(testQuestion.toJSON())
+    }
+    
     }
 }
 
