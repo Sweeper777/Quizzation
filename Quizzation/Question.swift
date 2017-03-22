630bae797c73e74797018dda60cd9910301f443c
Sweeper777 <sumulang@gmail.com>
2017-03-22 06:48:40 +0000

    added question class

--- Quizzation/Question.swift
+++ Quizzation/Question.swift
@@ -1,5 +1,36 @@
 import SwiftyJSON
 import Foundation
+
+class Question {
+    let type: QuestionType
+    let questionText: String
+    let possibleAnswers: [String]
+    
+    init(questionText: String, possibleAnswers: [String], hint: String? = nil) {
+        self.type = .blank
+        self.questionText = questionText
+        self.possibleAnswers = possibleAnswers
+    }
+    
+    init(questionText: String, answers: [MultipleChoiceAnswer], hint: String? = nil, requiresAllAnswers: Bool) {
+        if requiresAllAnswers {
+            self.type = .multipleMC
+            self.possibleAnswers = [answers.map { $0.rawValue.description }.sorted().joined(separator: "")]
+        } else {
+            self.type = .singleMC
+            self.possibleAnswers = answers.map { $0.rawValue.description }
+        }
+        self.questionText = questionText
+    }
+    
+    private init(type: QuestionType, questionText: String, possibleAnswers: [String], hint: String?) {
+        self.type = type
+        self.questionText = questionText
+        self.possibleAnswers = possibleAnswers
+    }
+    
+}
+
 enum QuestionType: Int {
     case blank
     case singleMC
