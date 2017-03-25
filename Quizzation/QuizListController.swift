23d21001b7fb985238655f1424f454db95b361b8
Sweeper777 <sumulang@gmail.com>
2017-03-25 08:53:30 +0000

    renamed view controller

--- Quizzation/QuizListController.swift
+++ Quizzation/QuizListController.swift
@@ -1,7 +1,6 @@
 import UIKit
-import Eureka
 
-class ViewController: FormViewController {
+class QuizListController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
