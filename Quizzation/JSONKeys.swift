a393cca1242060bf4fdd2f1893e73aaa7d4dae82
Sweeper777 <sumulang@gmail.com>
2017-03-27 07:45:13 +0100

    added new JSON keys

--- Quizzation/JSONKeys.swift
+++ Quizzation/JSONKeys.swift
@@ -2,3 +2,6 @@ let questionTypeKey = "type"
 let questionTextKey = "questionText"
 let possibleAnswersKey = "possibleAnswers"
 let hintKey = "hint"
+
+let questionNameKey = "name"
+let questionsKey = "questions"
