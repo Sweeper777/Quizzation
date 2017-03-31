9df9f20619d40581de56c28d823ced9c970c3aac
Sweeper777 <sumulang@gmail.com>
2017-03-31 07:25:33 +0100

    tested converting whole quiz to base64 string

--- Quizzation/QuizListController.swift
+++ Quizzation/QuizListController.swift
@@ -8,8 +8,22 @@ class QuizListController: UITableViewController {
     }
     
     func test1() {
-        let testQuestion = Question(questionText: "Hello", possibleAnswers: ["a", "b"])
-        print(testQuestion.toJSON())
+        let testQuestion1 = Question(questionText: "What is my name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
+        let testQuestion2 = Question(questionText: "What is his name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
+        let testQuestion3 = Question(questionText: "What is her name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
+        let testQuestion4 = Question(questionText: "What is your name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
+        let quiz = Quiz(name: "My Quiz", questions: [testQuestion1, testQuestion2, testQuestion3, testQuestion4])
+        let data = try! quiz.toJSON().rawData()
+        
+        print(data.base64EncodedString().characters.count)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
     }
     
     func test2() {
