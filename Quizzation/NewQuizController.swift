bc8097812afe60d856af8a1fcf503749c14ba518
Sweeper777 <sumulang@gmail.com>
2017-04-07 08:34:47 +0100

    tested question row

--- Quizzation/NewQuizController.swift
+++ Quizzation/NewQuizController.swift
@@ -11,6 +11,11 @@ class NewQuizController: FormViewController {
             row.title = NSLocalizedString("Name", comment: "")
             row.cell.textField.textAlignment = .left
         }
+        
+        form +++ QuestionRow("test") {
+            row in
+            
+            row.value = Question(questionText: "What is my name?", possibleAnswers: ["Sweeper"])
+        }
     }
-
 }
