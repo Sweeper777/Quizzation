7245e5d1fc0216562b2af8070d60a18e4a6217cf
Sweeper777 <sumulang@gmail.com>
2017-04-25 07:40:32 +0100

    deleted a test method

--- Quizzation/QuizListController.swift
+++ Quizzation/QuizListController.swift
@@ -12,17 +12,6 @@ class QuizListController: UITableViewController {
         quizzes = realm?.objects(QuizData.self)
     }
     
-    func test1() {
-        let testQuestion1 = Question(questionText: "What is my name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
-        let testQuestion2 = Question(questionText: "What is his name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
-        let testQuestion3 = Question(questionText: "What is her name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
-        let testQuestion4 = Question(questionText: "What is your name?", possibleAnswers: ["Sweeper", "Sweper", "Swooper", "Swaaper"])
-        let quiz = Quiz(name: "My Quiz", questions: [testQuestion1, testQuestion2, testQuestion3, testQuestion4])
-        let data = try! quiz.toJSON().rawData()
-        
-        print(data.base64EncodedString().characters.count)
-    }
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
