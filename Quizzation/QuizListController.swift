36dd76b605f55ce4720a9c1d5cf6fe638f5c07d5
Sweeper777 <sumulang@gmail.com>
2017-05-01 08:02:50 +0100

    used MGSwipeTableCells

--- Quizzation/QuizListController.swift
+++ Quizzation/QuizListController.swift
@@ -2,8 +2,9 @@ import UIKit
 import Base64nl
 import RealmSwift
 import SwiftyJSON
+import MGSwipeTableCell
 
-class QuizListController: UITableViewController {
+class QuizListController: UITableViewController, MGSwipeTableCellDelegate {
     let realm = try? Realm()
     var quizzes: Results<QuizData>!
     
@@ -21,14 +22,22 @@ class QuizListController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! MGSwipeTableCell
         let quizData = quizzes[indexPath.row].data!
         let json = JSON(data: quizData as Data)
         cell.textLabel?.text = json[quizNameKey].string
+        
+        cell.rightButtons = [MGSwipeButton(title: NSLocalizedString("Edit", comment: ""), backgroundColor: .green)]
+        cell.delegate = self
+        
         return cell
     }
     @IBAction func unwindFromNewQuiz(segue: UIStoryboardSegue) {
         tableView.reloadData()
+    }
+    
+    func swipeTableCell(_ cell: MGSwipeTableCell, tappedButtonAt index: Int, direction: MGSwipeDirection, fromExpansion: Bool) -> Bool {
+        return true
     }
 }
 
