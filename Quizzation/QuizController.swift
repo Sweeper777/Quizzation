ee3fc712cd18411f974b455278167ce90054d96e
Sweeper777 <sumulang@gmail.com>
2017-05-11 07:17:33 +0100

    added question text row

--- Quizzation/QuizController.swift
+++ Quizzation/QuizController.swift
@@ -6,6 +6,21 @@ class QuizController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for (index, question) in quiz.questions.enumerated() {
+            let questionSection = Section(NSLocalizedString("Question", comment: "") + " \(index)/\(quiz.questions.count)")
+            questionSection <<< LabelRow() {
+                row in
+                row.cell.textLabel?.numberOfLines = 0
+                row.title = question.questionText
+            }
+            form +++ questionSection
+            switch question.type {
+            case .blank:
+            case .multipleMC:
+            case .singleMC:
+            }
+        }
     }
 
     @IBAction func done() {
