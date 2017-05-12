7aafee8007f2bb3b47a2cf1ecf8f60e2820679c4
Sweeper777 <sumulang@gmail.com>
2017-05-12 07:10:37 +0100

    added export text in export form

--- Quizzation/ExportController.swift
+++ Quizzation/ExportController.swift
@@ -1,11 +1,20 @@
 import UIKit
 import Eureka
+import Base64nl
 
 class ExportController: FormViewController {
     var quizData: Data!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        form +++ Section(header: "", footer: NSLocalizedString("Share this and others can import the quiz using the code", comment: ""))
+            <<< LabelRow(tagExportText) {
+                row in
+                row.cell.textLabel?.numberOfLines = 0
+                row.cell.textLabel?.font = UIFont(name: "Courier New", size: 16)
+                row.title = quizData.base64EncodedString()
+        }
     }
 
 }
