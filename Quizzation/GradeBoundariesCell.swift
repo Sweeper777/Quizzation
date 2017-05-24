987a79c1e11a5befbf4521488d4938f34ba72c83
Sweeper777 <sumulang@gmail.com>
2017-05-24 06:55:33 +0100

    implemented update method

--- Quizzation/GradeBoundariesCell.swift
+++ Quizzation/GradeBoundariesCell.swift
@@ -13,6 +13,16 @@ public class GradeBoundariesCell: Cell<NSMutableArray>, CellType {
     
     public override func update() {
         super.update()
+        if let rowValue = row.value {
+            let values = rowValue.map { $0 as! Int }
+            multiSlider.value = values.map { CGFloat($0) }
+            var textViewText = "A*: \(Int(values[4]))%\n"
+            textViewText += "A: \(Int(values[3]))%\n"
+            textViewText += "B: \(Int(values[2]))%\n"
+            textViewText += "C: \(Int(values[1]))%\n"
+            textViewText += "D: \(Int(values[0]))%"
+            textView.text = textViewText
+        }
     }
     
     @IBAction func valueChange(_ sender: MultiSlider) {
