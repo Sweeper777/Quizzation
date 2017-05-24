15f6c17db20e68b7af6bf5030fbd9c65ac091a74
Sweeper777 <sumulang@gmail.com>
2017-05-24 06:55:22 +0100

    implemented valueChanged

--- Quizzation/GradeBoundariesCell.swift
+++ Quizzation/GradeBoundariesCell.swift
@@ -16,5 +16,13 @@ public class GradeBoundariesCell: Cell<NSMutableArray>, CellType {
     }
     
     @IBAction func valueChange(_ sender: MultiSlider) {
+        row.value = NSMutableArray(array: sender.value.map { Int($0) })
+        var textViewText = "A*: \(Int(sender.value[4]))%\n"
+        textViewText += "A: \(Int(sender.value[3]))%\n"
+        textViewText += "B: \(Int(sender.value[2]))%\n"
+        textViewText += "C: \(Int(sender.value[1]))%\n"
+        textViewText += "D: \(Int(sender.value[0]))%"
+        textView.text = textViewText
+    }
     }
 }
