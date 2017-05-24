d3b86836d34d23ab066e04bbeea016649eb545ad
Sweeper777 <sumulang@gmail.com>
2017-05-24 06:53:56 +0100

    added grade boundaries property in quiz class

--- Quizzation/Quiz.swift
+++ Quizzation/Quiz.swift
@@ -3,24 +3,28 @@ import SwiftyJSON
 
 class Quiz {
     let name: String
+    let gradeBoundaries: [Int]
     let questions: [Question]
     
-    init(name: String, questions: [Question]) {
+    init(name: String, questions: [Question], gradeBoundaries: [Int]) {
         self.name = name
         self.questions = questions
+        self.gradeBoundaries = gradeBoundaries
     }
     
     func toJSON() -> JSON {
         return [
             quizNameKey: name,
-            questionsKey: questions.map { $0.toJSON() }
+            questionsKey: questions.map { $0.toJSON() },
+            gradeBoundariesKey: gradeBoundaries
         ]
     }
     
     static func from(json: JSON) -> Quiz? {
         guard let name = json[quizNameKey].string else { return nil }
         guard let arr = json[questionsKey].array else { return nil }
+        guard let gradeBoundaries = (json[gradeBoundariesKey].array?.map { $0.int }.filter { $0 != nil }) else { return nil }
         let questions = arr.map { Question.from(json: $0) }.filter { $0 != nil }
-        return Quiz(name: name, questions: questions as! [Question])
+        return Quiz(name: name, questions: questions as! [Question], gradeBoundaries: gradeBoundaries as! [Int])
     }
 }
