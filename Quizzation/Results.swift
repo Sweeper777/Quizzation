fe5552d6ffb277ff5306f508c284fdf3e39d2693
Sweeper777 <sumulang@gmail.com>
2017-05-28 08:49:24 +0100

    added WronglyAnsweredQuestion struct

--- Quizzation/Results.swift
+++ Quizzation/Results.swift
@@ -2,4 +2,11 @@ struct Results {
     let quiz: Quiz
     let correctAnswers: Int
     let hintUsed: Int
+    let wrongAnswers: [WronglyAnsweredQuestion]
+}
+
+struct WronglyAnsweredQuestion {
+    let questionText: String
+    let correctAnswers: [String]
+    let yourAnswer: String
 }
