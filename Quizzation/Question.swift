f304a7a55289d03d271436095159671114835dbc
Sweeper777 <sumulang@gmail.com>
2017-07-06 06:50:44 +0100

    removed old toJSON and fromJSON in Question

--- Quizzation/Question.swift
+++ Quizzation/Question.swift
@@ -14,25 +14,8 @@ class Question: Equatable {
         self.hint = hint
     }
     
-    func toJSON() -> JSON {
-        return [
-            questionTypeKey: type.rawValue,
-            questionTextKey: questionText,
-            possibleAnswersKey: possibleAnswers,
-            hintKey: hint as Any
-        ]
     }
     
-    static func from(json: JSON) -> Question? {
-        guard let type = json[questionTypeKey].int else { return nil }
-        guard let questionText = json[questionTextKey].string else { return nil }
-        guard let possibleAnswersJSON = json[possibleAnswersKey].array else { return nil }
-        let possibleAnswers = possibleAnswersJSON.map { $0.string }.filter { $0 != nil }.map { $0! }
-        if possibleAnswers.isEmpty {
-            return nil
-        }
-        
-        return Question(type: QuestionType(rawValue: type)!, questionText: questionText, possibleAnswers: possibleAnswers, hint: json[hintKey].string)
     }
     
     static func ==(lhs: Question, rhs: Question) -> Bool {
